1bc4f9885815e0af69fef927814c324c4bad58b3
Romano_M <deep.depp@yandex.com>
2020-09-02 12:57:33 +0300

    Add test logging code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        Chrono chrono = new Chrono(1);
+        PeriodPrinter secondsPrinter = new PeriodPrinter(chrono);
+        List<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        WordsPrinter wordsPrinter = new WordsPrinter(words, 5, chrono);
+        WordsPrinter wordsPrinter2 = new WordsPrinter(words, 7, chrono);
 
+        new Thread(chrono).start();
+        new Thread(secondsPrinter).start();
+        new Thread(wordsPrinter).start();
+        new Thread(wordsPrinter2).start();
     }
 }
